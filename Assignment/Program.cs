5d7bf27d75e8cc0b2baa4e1ddccc9f7cb54a1775
Haidy08 <Haidi.sobhi.22@gmail.com>
2024-06-24 04:33:10 +0300

    1.Write a program that allows the user to enter a number then print it.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,11 +1,21 @@
 ﻿namespace Assignment
 {
-     class Program
+    class Program
     {
-       public static void Main()
+        public static void Main()
         {
-            
 
+            //Assignment 2
+
+
+            #region 1.Write a program that allows the user to enter a number then print it.
+
+            /*Console.Write("Please Enter Number ");
+               int Num = Convert.ToInt32(Console.ReadLine());
+               Console.Clear();
+               Console.WriteLine($"Please Enter Number{Num}");*/
+
+            #endregion
 
 
         }
